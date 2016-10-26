57c17a644e78418ef9e17b27e1f1eaa91bd571a4
Pushkar Piggott <pushkarkp@gmail.com>
2016-10-27 07:12:25 +1100

    persistent radio buttons

--- pkp/ui/PersistentDialog.java
+++ pkp/ui/PersistentDialog.java
@@ -7,7 +7,11 @@ package pkp.ui;
 
 import java.awt.Window;
 import java.awt.Rectangle;
+import java.awt.Component;
 import javax.swing.JDialog;
+import javax.swing.Box;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
 import pkp.util.Persist;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -48,6 +52,42 @@ public class PersistentDialog extends JDialog {
       Persist.set(persistName + ".w", Integer.toString(r.width));
       Persist.set(persistName + ".h", Integer.toString(r.height));
       super.dispose();
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   public void persist(Box p) {
+      int n = 0;
+      boolean inRb = false;
+      for (int j = 0; j < p.getComponentCount(); ++j) {
+         if (p.getComponent(j) instanceof JRadioButton) {
+            if (!inRb) {
+               inRb = true;
+               ++n;
+            }
+            JRadioButton rb = (JRadioButton)p.getComponent(j);
+            if (rb.isSelected()) {
+               Persist.set(getPersistName() + "." + n, Persist.toTag("#." + n + "." + rb.getActionCommand()));
+            }
+         } else {
+            inRb = false;
+         }
+      }
+   }
+
+   ///////////////////////////////////////////////////////////////////
+   protected JRadioButton createRadioButton(String text, ButtonGroup buttonGroup) {
+      boolean selected = false;
+      for (int i = 1; !selected; ++i) {
+         String selection = Persist.get(getPersistName() + ' ' + i);
+         if (selection == null) {
+            break;
+         }
+         selected = Persist.match("#." + selection, "#." + i + "." + text);
+      }
+      JRadioButton rb = new JRadioButton(text, selected);
+      rb.setOpaque(false);
+      buttonGroup.add(rb);
+      return rb;
    }
 
    // Private /////////////////////////////////////////////////////////////////
